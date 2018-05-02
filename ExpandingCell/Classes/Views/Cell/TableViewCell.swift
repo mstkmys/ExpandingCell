83b3871bf9cc11c702817287c7db46ff9d2a952b
Masataka Miyoshi <masataka.ehime.univ@gmail.com>
2018-05-03 06:13:10 +0900

    カスタムセルを作成して、TableViewControllerにNibとして設定

---
+++ ExpandingCell/Classes/Views/Cell/TableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  TableViewCell.swift
+//  ExpandingCell
+//
+//  Created by Miyoshi Masataka on 2018/05/03.
+//  Copyright © 2018年 Masataka Miyoshi. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
